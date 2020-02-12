c2fd1386ad70ce98188569e185e9a7130085d652
jamesgrubb <jamesgrubb@mac.com>
2020-02-12 21:40:28 +0000

    fixing case

---
+++ web/src/components/layout/Layout.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { MusicPlayerProvider } from "../../context/MusicPlayerContext"
+
+import "./styles.scss"
+
+const Layout = ({ children }) => {
+  return (
+    <MusicPlayerProvider>
+      <div className="container">
+        <header className="header">
+          <h1 className="header__title">Rupert Gibson</h1>
+          <nav className="header__nav">
+            <ul className="header__navItems">
+              <li>about</li>
+              <li>contact</li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+      </div>
+    </MusicPlayerProvider>
+  )
+}
+
+export default Layout
