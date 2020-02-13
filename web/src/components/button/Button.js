3546017ed60ce2be65b413ee605b503254c49fd7
jamesgrubb <jamesgrubb@mac.com>
2020-02-13 14:00:10 +0000

    css tweaks

--- web/src/components/button/Button.js
+++ web/src/components/button/Button.js
@@ -8,7 +8,10 @@ const Button = ({ handlePlayTrack, className }) => {
         <svg
           className="button"
           xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
           viewBox="0,0,100,100"
+          width="100"
+          height="100"
         >
           <path className={className} />
         </svg>
